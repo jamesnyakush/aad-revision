63b41f11ad85c79722b9c6e7103732fe38ac7ddf
jamesnyakush <emvesttech@gmail.com>
2019-12-09 12:12:56 +0300

    learned snackbar

--- app/src/main/java/com/jamesnyakush/aadrevision/MainActivity.kt
+++ app/src/main/java/com/jamesnyakush/aadrevision/MainActivity.kt
@@ -2,16 +2,38 @@ package com.jamesnyakush.aadrevision
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var snackbar: Snackbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        snack_click.setOnClickListener(this)
 
         // this is a toast
-        Toast.makeText(this,"Welcome To AAD Certification Exams",Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Welcome To AAD Certification Exams", Toast.LENGTH_LONG).show()
     }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.snack_click -> {
+                snackbar = Snackbar.make(
+                    snack_layout,
+                    "This is An AAD Snackbar Revision",
+                    Snackbar.LENGTH_LONG
+                )
+
+                snackbar.show()
+            }
+        }
+    }
+
+
 }
